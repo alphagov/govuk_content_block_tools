b5cc0e1a05552fb26e2838ac80e2e95666fde468
pezholio <pezholio@gmail.com>
2024-11-15 15:29:12 +0000

    Improve documentation

--- lib/content_block_tools/content_block_reference.rb
+++ lib/content_block_tools/content_block_reference.rb
@@ -1,7 +1,35 @@
 module ContentBlockTools
-  class ContentBlockReference < Data.define(:document_type, :content_id, :embed_code)
+  ContentBlockReference = Data.define(:document_type, :content_id, :embed_code)
+
+  # Defines a reference pointer for a Content Block
+  #
+  # @api public
+  # @!attribute [r] document_type
+  #   The document type of the content block - this will be used to work out which Presenter
+  #   will be used to render the content block. All supported document_types are documented in
+  #   {ContentBlockTools::ContentBlockReference::SUPPORTED_DOCUMENT_TYPES}
+  #   @example
+  #     content_block_reference.document_type #=> "content_block_email_address"
+  #   @return [String] the document type
+  #   @api public
+  #
+  # @!attribute [r] content_id
+  #   The content UUID for a block
+  #   @example
+  #     content_block_reference.id #=> "2b92cade-549c-4449-9796-e7a3957f3a86"
+  #   @return [String]
+  #
+  # @!attribute [r] embed_code
+  #   The embed_code used for a block
+  #   @example
+  #     content_block_reference.embed_code #=> "{{embed:content_block_email_address:2b92cade-549c-4449-9796-e7a3957f3a86}}"
+  #   @return [String]
+  class ContentBlockReference < Data
+    # An array of the supported document types
     SUPPORTED_DOCUMENT_TYPES = %w[contact content_block_email_address].freeze
+    # The regex used to find UUIDs
     UUID_REGEX = /([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})/
+    # The regex used when scanning a document using {ContentBlockTools::ContentBlockReference.find_all_in_document}
     EMBED_REGEX = /({{embed:(#{SUPPORTED_DOCUMENT_TYPES.join('|')}):#{UUID_REGEX}}})/
 
     class << self

d3fd0659539f80e7268657297200c85234133ef7
pezholio <pezholio@gmail.com>
2024-11-15 14:29:51 +0000

    Add an EmailAddressPresenter
    This inherits from the BasePresenter and returns the email address

--- lib/content_block_tools.rb
+++ lib/content_block_tools.rb
@@ -6,6 +6,7 @@ require "content_block_tools/content_block"
 require "content_block_tools/content_block_reference"
 
 require "content_block_tools/presenters/base_presenter"
+require "content_block_tools/presenters/email_address_presenter"
 
 require "content_block_tools/version"
 

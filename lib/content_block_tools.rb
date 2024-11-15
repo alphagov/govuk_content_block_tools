11d10f1e87e600c9c9a96f9ec1af9a4f05ea1927
pezholio <pezholio@gmail.com>
2024-11-15 09:40:26 +0000

    Add Rubocop GOV.UK rules

--- lib/content_block_tools.rb
+++ lib/content_block_tools.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require "content_block_tools/version"
 
 module ContentBlockTools
--- spec/content_block_tools_spec.rb
+++ spec/content_block_tools_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 RSpec.describe ContentBlockTools do
   it "has a version number" do
     expect(ContentBlockTools::VERSION).not_to be nil

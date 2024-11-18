b05dd10d6fadd4b176b7a68ff84b74d90bb4d287
pezholio <pezholio@gmail.com>
2024-11-18 09:13:30 +0000

    Add a `BasePresenter`
    In future commits, we’ll have a presenter that inherits from this one.
    All presenters will render a base tag with data attributes that contain
    information about the content block. Presenters that inherit from the
    BasePresenter will be able to override the `content` method and return
    the relevant information from the block in the format they need.

--- lib/content_block_tools.rb
+++ lib/content_block_tools.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+require "action_view"
+require "uri"
+require "content_block_tools/presenters/base_presenter"
 require "content_block_tools/content_block_reference"
 
 require "content_block_tools/version"
@@ -7,4 +10,5 @@ require "content_block_tools/version"
 module ContentBlockTools
   class Error < StandardError; end
   # Your code goes here...
+  module Presenters; end
 end

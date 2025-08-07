9f00f1471a341e429768ab51108faa7588b2dd87
pezholio <pezholio@gmail.com>
2025-08-07 09:00:21 +0100

    Add some basic styling
    This will allow us to use custom styling in our content blocks.

--- lib/content_block_tools/engine.rb
+++ lib/content_block_tools/engine.rb
@@ -2,6 +2,13 @@ module ContentBlockTools
   class Engine < ::Rails::Engine
     isolate_namespace ContentBlockTools
 
+    initializer "content_block_tools.assets" do
+      Rails.application.config.assets.paths += %w[
+        app/assets/stylesheets
+        node_modules/govuk-frontend/dist/govuk/core
+      ]
+    end
+
     initializer "content_block_tools.initialize_logger" do
       ContentBlockTools.logger = Rails.logger
     end

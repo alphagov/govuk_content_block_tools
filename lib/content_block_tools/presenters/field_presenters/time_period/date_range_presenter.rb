f33b0ddfcecb1da303ded9eff2b34f4feb9639f7
Ed Davey <ed.davey@digital.cabinet-office.gov.uk>
2026-04-01 15:52:58 +0100

    Refactor DateRangePresenter to use NormalisedDateRange
    As in the previous commit we replace direct digging into field hash with
    NormalisedDateRange, enabling the presenter to accept both legacy and
    ISO 8601 date formats.

--- lib/content_block_tools/presenters/field_presenters/time_period/date_range_presenter.rb
+++ lib/content_block_tools/presenters/field_presenters/time_period/date_range_presenter.rb
@@ -5,25 +5,38 @@ module ContentBlockTools
         class TimePeriodPresenterError < RuntimeError; end
 
         class DateRangePresenter < BasePresenter
+          def initialize(field, **args)
+            super
+            @normalised_date_range = normalise_date_range
+          end
+
           def render
             return unless start_date.present? && end_date.present?
 
             "#{start_date} to #{end_date}"
-          rescue Date::Error, TypeError
-            raise TimePeriodPresenterError, "Not a valid date range: #{field}"
+          rescue NormalisedDateRange::ParseError => e
+            raise TimePeriodPresenterError, "Not a valid date range: #{field} (#{e.message})"
           end
 
         private
 
+          attr_reader :normalised_date_range
+
           def start_date
-            presented_date(field.dig(:start, :date))
+            presented_date(normalised_date_range.start_date)
           end
 
           def end_date
-            presented_date(field.dig(:end, :date))
+            presented_date(normalised_date_range.end_date)
+          end
+
+          def normalise_date_range
+            NormalisedDateRange.new(field)
           end
 
           def presented_date(date)
+            return unless date.present?
+
             Presenters::FieldPresenters::TimePeriod::DatePresenter.new(
               date,
             ).render
--- spec/content_block_tools/presenters/field_presenters/time_period/date_range_presenter_spec.rb
+++ spec/content_block_tools/presenters/field_presenters/time_period/date_range_presenter_spec.rb
@@ -1,21 +1,61 @@
 RSpec.describe ContentBlockTools::Presenters::FieldPresenters::TimePeriod::DateRangePresenter do
-  let(:date_range) do
-    {
-      start: {
-        date: "2025-04-06",
-        time: "00:00",
-      },
-      end: {
-        date: "2026-04-05",
-        time: "23:59",
-      },
-    }
+  describe "with legacy format" do
+    let(:date_range) do
+      {
+        start: {
+          date: "2025-04-06",
+          time: "00:00",
+        },
+        end: {
+          date: "2026-04-05",
+          time: "23:59",
+        },
+      }
+    end
+
+    it "presents a date range in words, omitting time" do
+      presenter = described_class.new(date_range)
+
+      expect(presenter.render).to eq("6 April 2025 to 5 April 2026")
+    end
   end
 
-  it "presents a date range in words, omitting time" do
-    presenter = described_class.new(date_range)
+  describe "with ISO 8601 format" do
+    let(:date_range) do
+      {
+        start: "2025-04-06T00:00:00+00:00",
+        end: "2026-04-05T23:59:00+00:00",
+      }
+    end
 
-    expect(presenter.render).to eq("6 April 2025 to 5 April 2026")
+    it "presents a date range in words, omitting time" do
+      presenter = described_class.new(date_range)
+
+      expect(presenter.render).to eq("6 April 2025 to 5 April 2026")
+    end
+  end
+
+  describe "format consistency" do
+    let(:legacy_format) do
+      {
+        start: { date: "2025-04-06", time: "00:00" },
+        end: { date: "2026-04-05", time: "23:59" },
+      }
+    end
+
+    let(:iso8601_format) do
+      {
+        start: "2025-04-06T00:00:00+00:00",
+        end: "2026-04-05T23:59:00+00:00",
+      }
+    end
+
+    it "produces identical output for both formats" do
+      legacy = described_class.new(legacy_format)
+      iso8601 = described_class.new(iso8601_format)
+
+      expect(legacy.render).to eq(iso8601.render)
+    end
   end
 
   context "when the date range is not defined" do
@@ -44,7 +84,7 @@ RSpec.describe ContentBlockTools::Presenters::FieldPresenters::TimePeriod::DateR
       end
     end
 
-    context "when the value found can't be parsed as a date" do
+    context "when the legacy format date can't be parsed" do
       let(:date_range) do
         {
           start: {
@@ -63,7 +103,25 @@ RSpec.describe ContentBlockTools::Presenters::FieldPresenters::TimePeriod::DateR
 
         expect { presenter.render }.to raise_error(
           ContentBlockTools::Presenters::FieldPresenters::TimePeriod::TimePeriodPresenterError,
-          "Not a valid date range: #{date_range}",
+          /Not a valid date range:.*Invalid legacy date format/,
+        )
+      end
+    end
+
+    context "when the ISO 8601 format is invalid" do
+      let(:date_range) do
+        {
+          start: "not-a-date",
+          end: "2026-04-05T23:59:00+00:00",
+        }
+      end
+
+      it "raises an error with an informative message" do
+        presenter = described_class.new(date_range)
+
+        expect { presenter.render }.to raise_error(
+          ContentBlockTools::Presenters::FieldPresenters::TimePeriod::TimePeriodPresenterError,
+          /Not a valid date range:.*Invalid ISO 8601 format/,
         )
       end
     end

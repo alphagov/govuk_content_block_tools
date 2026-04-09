06e8eb333f20b0769f55c3389c951ffb9f1bfa77
Ed Davey <ed.davey@digital.cabinet-office.gov.uk>
2026-04-09 09:48:51 +0100

    Present date from Time obj as well as str representation
    Time period is now using NormalisedDateRange to parse date or datetime
    strings into Time objects.
    
    So we adapt our DatePresenter to:
    
    - format a Time object
    - parse a string representation if given (we use Date.parse for
    stricter parsing)

--- spec/content_block_tools/presenters/field_presenters/time_period/date_presenter_spec.rb
+++ spec/content_block_tools/presenters/field_presenters/time_period/date_presenter_spec.rb
@@ -1,13 +1,57 @@
 RSpec.describe ContentBlockTools::Presenters::FieldPresenters::TimePeriod::DatePresenter do
-  it "presents a date" do
-    date = "2023-01-01"
+  context "when the given param is a string representation" do
+    context "and the string is a well formed date" do
+      it "presents a date" do
+        string = "2023-01-01"
 
-    presenter = described_class.new(date)
+        presenter = described_class.new(string)
 
-    expect(presenter.render).to eq("1 January 2023")
+        expect(presenter.render).to eq("1 January 2023")
+      end
+    end
+
+    context "and the string is a well formed time" do
+      it "presents a date" do
+        string = "2023-01-01T00:00:00+00:00"
+
+        presenter = described_class.new(string)
+
+        expect(presenter.render).to eq("1 January 2023")
+      end
+    end
+
+    context "and the string is badly formed" do
+      it "returns nil" do
+        string = "not-a-date"
+
+        presenter = described_class.new(string)
+
+        expect(presenter.render).to be_nil
+      end
+    end
+
+    context "and the string is an impossible date" do
+      it "returns nil" do
+        string = "2025-02-30"
+
+        presenter = described_class.new(string)
+
+        expect(presenter.render).to be_nil
+      end
+    end
   end
 
-  context "when the date is not defined" do
+  context "when the given param is a Time object (via NormalisedDateRange)" do
+    it "presents a date" do
+      time = Time.zone.parse("2023-01-01T000:00:00+00:00")
+
+      presenter = described_class.new(time)
+
+      expect(presenter.render).to eq("1 January 2023")
+    end
+  end
+
+  context "when the param is nil" do
     it "renders nothing" do
       presenter = described_class.new(nil)
 

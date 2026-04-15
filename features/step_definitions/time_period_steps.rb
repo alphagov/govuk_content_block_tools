9ab1c3a94ef3579ead700b2f2cf8243964cd2e0d
Ed Davey <ed.davey@digital.cabinet-office.gov.uk>
2026-04-15 17:02:36 +0100

    Describe TimePeriod format rendering with cucumber

--- features/step_definitions/time_period_steps.rb
+++ features/step_definitions/time_period_steps.rb
@@ -1,3 +1,42 @@
+Given("a time period content block with the following date range:") do |table|
+  details = table.rows_hash
+  @content_block_details = {
+    date_range: {
+      start: details.fetch("start"),
+      end: details.fetch("end"),
+    },
+  }
+end
+
+When("asked to render with embed code {string}") do |embed_code|
+  @embed_code = embed_code
+  @content_block = ContentBlockTools::ContentBlock.new(
+    document_type: "content_block_time_period",
+    content_id: SecureRandom.uuid,
+    title: "Tax year",
+    details: @content_block_details,
+    embed_code:,
+  )
+
+  begin
+    @rendered = @content_block.render
+    @error = nil
+  rescue ContentBlockTools::InvalidFormatError => e
+    @error = e
+    @rendered = nil
+  end
+end
+
+Then("the rendered output should be {string}") do |expected_text|
+  expect(@error).to be_nil, "Expected no error but got: #{@error&.message}"
+  expect(@rendered).to include(expected_text)
+end
+
+Then("an InvalidFormatError should be raised with message {string}") do |expected_message|
+  expect(@error).to be_a(ContentBlockTools::InvalidFormatError)
+  expect(@error.message).to eq(expected_message)
+end
+
 Given("a time period content block exists") do
   @content_block = ContentBlockTools::ContentBlock.new(
     document_type: "content_block_time_period",

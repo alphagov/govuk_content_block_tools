ec67aa8c51289fa27b9c5d4b0298f9fa893a59b1
Ed Davey <ed.davey@digital.cabinet-office.gov.uk>
2026-04-16 18:50:40 +0100

    Remove EmbedCode#field_names in favour of internal_content_path
    We:
    
    - replace the `#field_names` method with `#internal_content_path` as
    the sole public API for accessing the path to internal content.
    
    - rename the internal `#parse_field_names` to `#parse_path_segments` and
    convert_field_item to convert_segment to reflect the new terminology.

--- spec/content_block_tools/embed_code_spec.rb
+++ spec/content_block_tools/embed_code_spec.rb
@@ -1,10 +1,16 @@
 RSpec.describe ContentBlockTools::EmbedCode do
-  describe "#field_names" do
+  describe "#internal_content_path" do
+    subject(:path) { described_class.new(embed_code).internal_content_path }
+
     context "when embed code has no field path" do
       let(:embed_code) { "{{embed:content_block_contact:main-office}}" }
 
-      it "returns an empty array" do
-        expect(described_class.new(embed_code).field_names).to eq([])
+      it "returns an empty path" do
+        expect(path.path).to eq([])
+      end
+
+      it "is not present" do
+        expect(path).not_to be_present
       end
     end
 
@@ -12,7 +18,11 @@ RSpec.describe ContentBlockTools::EmbedCode do
       let(:embed_code) { "{{embed:content_block_contact:main-office/email}}" }
 
       it "returns the field as a symbol" do
-        expect(described_class.new(embed_code).field_names).to eq([:email])
+        expect(path.path).to eq([:email])
+      end
+
+      it "is singular" do
+        expect(path).to be_singular
       end
     end
 
@@ -20,7 +30,12 @@ RSpec.describe ContentBlockTools::EmbedCode do
       let(:embed_code) { "{{embed:content_block_contact:main-office/email_addresses/main}}" }
 
       it "returns all fields as symbols" do
-        expect(described_class.new(embed_code).field_names).to eq(%i[email_addresses main])
+        expect(path.path).to eq(%i[email_addresses main])
+      end
+
+      it "exposes block_type and block_name" do
+        expect(path.block_type).to eq(:email_addresses)
+        expect(path.block_name).to eq(:main)
       end
     end
 
@@ -28,33 +43,16 @@ RSpec.describe ContentBlockTools::EmbedCode do
       let(:embed_code) { "{{embed:content_block_contact:main-office/email_addresses/0/email}}" }
 
       it "converts numeric segments to integers" do
-        expect(described_class.new(embed_code).field_names).to eq([:email_addresses, 0, :email])
+        expect(path.path).to eq([:email_addresses, 0, :email])
       end
     end
 
     context "when embed code is invalid" do
       let(:embed_code) { "invalid" }
 
-      it "returns an empty list" do
-        expect(described_class.new(embed_code).field_names).to be_empty
+      it "returns an empty path" do
+        expect(path.path).to be_empty
       end
-    end
-  end
-
-  describe "#internal_content_path" do
-    it "returns an InternalContentPath with the field names" do
-      embed_code = described_class.new("{{embed:content_block_contact:main-office/email_addresses/main}}")
-      path = embed_code.internal_content_path
-
-      expect(path).to be_a(ContentBlockTools::InternalContentPath)
-      expect(path.path).to eq(%i[email_addresses main])
-    end
-
-    it "returns an empty InternalContentPath when no field path" do
-      embed_code = described_class.new("{{embed:content_block_contact:main-office}}")
-      path = embed_code.internal_content_path
-
-      expect(path).not_to be_present
     end
   end
 end
